4b0e439a52cc2647426abae56cd29d4116c35f90
OlegJakushkin <oleg.jakushkin@gmail.com>
2022-06-13 00:37:54 +0300

    Update install_scripts.sh

--- install_scripts.sh
+++ install_scripts.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-cd ./models/FlowNet2_Models/resample2d_package/
-rm -rf *_cuda.egg-info build dist __pycache__
-python3 setup.py develop
+#cd ./models/FlowNet2_Models/resample2d_package/
+#rm -rf *_cuda.egg-info build dist __pycache__
+#python3 setup.py develop
 
 cd ../correlation_package
 rm -rf *_cuda.egg-info build dist __pycache__
